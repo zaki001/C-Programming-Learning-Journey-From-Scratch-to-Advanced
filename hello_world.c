9526f02638f5fa6385aea589e4041ad3cd7d38e2
Zaki001 <ahmadzaki.af@gmail.com>
2024-03-20 22:32:36 -0400

    My first program in C programming language

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+// Include the standard input/output library for accessing printf function 
+#include <stdio.h>
+
+// Main function: the starting point of the program
+
+int main() {
+	// This print the "Hello, World!" to the console
+	printf("Hello, World!\n");
+
+	// Return zero to indicate that program finished successfully
+	return 0;
+}
